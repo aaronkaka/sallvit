5b8e2e447d8139e212a8551bf45764452ad0ed42
Aaron Kaka <aaron.kaka@pearson.com>
2015-01-21 22:13:15 -0700

    Updates for Android release, and added favicon. Also optimized relevant images.

--- javascripts/slidepanel.js
+++ javascripts/slidepanel.js
@@ -1,9 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
+    var slideAction = function () {
+        $("#panel").slideToggle("slow");
+        $(".btn-slide").toggleClass("active");
+    };
+
+    // Slide the notification into view immediately on page load
+    slideAction();
 
 	$(".btn-slide").click(function(){
-		$("#panel").slideToggle("slow");
-		$(this).toggleClass("active"); return false;
+        slideAction();
+        return false;
 	});
-	
 	 
 });
--- makezip.sh
+++ makezip.sh
@@ -1,1 +1,1 @@
-zip -r sallvit-landing.zip images javascripts stylesheets index.html about.html cybertrek.html
+zip -r sallvit-landing.zip images javascripts stylesheets index.html about.html cybertrek.html favicon.ico
